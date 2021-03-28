527a2a34295523b39fff1238769691e5ba3c0e61
Carlos Eduardo <BRMAZ0001@totvspartners.com.br>
2021-03-28 11:13:41 -0300

    #2 Added tests units in bid

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -1,7 +1,31 @@
 import Lance from '@/components/Lance'
-import { mount } from '@vue/test-utils'
+import { createWrapper, mount } from '@vue/test-utils'
 
-test('não aceita lance com valor menor do que zero', () => {
+test('does not accept when bid less than zero', () => {
     const wraper = mount(Lance)
-    expect(wraper).toBeTruthy()
+    const input = wraper.find('input')
+    input.setValue(-100)
+    wraper.trigger('submit')
+    const lancesEmitidios = wraper.emitted('novo-lance')
+    expect(lancesEmitidios).toBeUndefined()
+})
+
+test('accept a bid when the value is greater than zero', () => {
+    const wraper = mount(Lance)
+    const input = wraper.find('input')
+    input.setValue(100)
+    wraper.trigger('submit')
+    const lancesEmitidios = wraper.emitted('novo-lance')
+    expect(lancesEmitidios).toHaveLength(1)
+})
+
+test('emit the expected value of the bid valid', () => {
+    const wraper = mount(Lance)
+    const input = wraper.find('input')
+    input.setValue(100)
+    wraper.trigger('submit')
+    const lancesEmitidios = wraper.emitted('novo-lance')
+
+    const lance = parseInt(lancesEmitidios[0][0])
+    expect(lance).toBe(100)
 })
