0da8871b16f0445df91fa763ab6cb015798b51fa
Carlos Eduardo <BRMAZ0001@totvspartners.com.br>
2021-03-28 13:24:41 -0300

    #3 added tests units to bid, passing value minimum

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -1,31 +1,84 @@
 import Lance from '@/components/Lance'
 import { createWrapper, mount } from '@vue/test-utils'
 
-test('does not accept when bid less than zero', () => {
-    const wraper = mount(Lance)
-    const input = wraper.find('input')
-    input.setValue(-100)
-    wraper.trigger('submit')
-    const lancesEmitidios = wraper.emitted('novo-lance')
-    expect(lancesEmitidios).toBeUndefined()
+describe('A bid without value minimum', () => {
+    test('does not accept when bid less than zero', () => {
+        const wraper = mount(Lance)
+        const input = wraper.find('input')
+        input.setValue(-100)
+        wraper.trigger('submit')
+        const lancesEmitidios = wraper.emitted('novo-lance')
+
+        expect(lancesEmitidios).toBeUndefined()
+    })
+    
+    test('accept a bid when the value is greater than zero', () => {
+        const wraper = mount(Lance)
+        const input = wraper.find('input')
+        input.setValue(100)
+        wraper.trigger('submit')
+        const lancesEmitidios = wraper.emitted('novo-lance')
+
+        expect(lancesEmitidios).toHaveLength(1)
+    })
+    
+    test('emit the expected value of the bid valid', () => {
+        const wraper = mount(Lance)
+        const input = wraper.find('input')
+        input.setValue(100)
+        wraper.trigger('submit')
+        const lancesEmitidios = wraper.emitted('novo-lance')
+    
+        const lance = parseInt(lancesEmitidios[0][0])
+        expect(lance).toBe(100)
+    })
 })
 
-test('accept a bid when the value is greater than zero', () => {
-    const wraper = mount(Lance)
-    const input = wraper.find('input')
-    input.setValue(100)
-    wraper.trigger('submit')
-    const lancesEmitidios = wraper.emitted('novo-lance')
-    expect(lancesEmitidios).toHaveLength(1)
-})
+describe('A bid with value minimum', () => {
+    test('all spears must have a value greather than the minimum', () => {
+        const wraper = mount(Lance, {
+            propsData: {
+                lanceMinimo: 300
+            }
+        })
+        const input = wraper.find('input')
+        input.setValue(400)
+        wraper.trigger('submit')
+        const lancesEmitidios = wraper.emitted('novo-lance')
 
-test('emit the expected value of the bid valid', () => {
-    const wraper = mount(Lance)
-    const input = wraper.find('input')
-    input.setValue(100)
-    wraper.trigger('submit')
-    const lancesEmitidios = wraper.emitted('novo-lance')
+        expect(lancesEmitidios).toHaveLength(1)
+    })
 
-    const lance = parseInt(lancesEmitidios[0][0])
-    expect(lance).toBe(100)
+    test('emit the expected value of the bid valid', () => {
+        const wraper = mount(Lance, {
+            propsData: {
+                lanceMinimo: 300
+            }
+        })
+        const input = wraper.find('input')
+        input.setValue(400)
+        wraper.trigger('submit')
+        const lancesEmitidios = wraper.emitted('novo-lance')
+        
+        const lance = parseInt(lancesEmitidios[0][0])
+        expect(lance).toBe(400)
+    })
+
+    test('does not accept bid with value less than the value minimum', async () => {
+        const wraper = mount(Lance, {
+            propsData: {
+                lanceMinimo: 300
+            }
+        })
+        const input = wraper.find('input')
+        input.setValue(200)
+        wraper.trigger('submit')
+        // waiting for the DOM to be updated 
+        await wraper.vm.$nextTick()
+
+        const msgErro = wraper.find('p.alert').element.textContent
+        const msgEsperado = 'O valor mínimo para o lance é de R$ 300'
+        expect(msgErro).toBeTruthy()
+        expect(msgErro).toContain(msgEsperado)
+    })
 })
